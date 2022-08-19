72cd7a229b521db62f51edbb790eb8608a698db9
Sven Weidenmann <sven.weidenmann@1up.io>
2022-08-19 13:49:12 +0200

    eleventh commit

--- public/carb.php
+++ public/carb.php
@@ -4,7 +4,50 @@
 
 <?php include('templates/galleryCarbs.php'); ?>
 
+<div class="carbContainer">
 
+    <h1>Carb Bez.</h1>
+
+
+    <!-- Img -->
+    <div class="carbTabImg"> </div>
+
+    <!-- Nährstoffe -->
+    <div class="naherstoffe">
+        <h2>Nährstoffe</h2>
+
+        <!-- Vitamine -->
+        <h3>Vitamine</h3>
+        <ul>
+            <li>
+                <p>Vitamin A: 800 microgramm auf 100g</p>
+            </li>
+        </ul>
+
+        <!-- Mineralstoffe -->
+        <h3>Mineralstoffe</h3>
+        <ul>
+            <li>
+                <p>Kalium: 373mg auf 100g</p>
+            </li>
+        </ul>
+
+        <!-- Spurenelemente -->
+        <h3>Spurenelemente</h3>
+        <ul>
+            <li>
+                <p>Zink: 0.1mg auf 100g</p>
+            </li>
+        </ul>
+    </div>
+
+    <!-- Getreide/Reiss/Kartoffeln Art -->
+    <div class="carbArt">
+        <h2>Getreide Art </h2>
+        <p>Text</p>
+    </div>
+
+</div>
 
 
 <?php include('templates/footer.php'); ?>
--- public/prot.php
+++ public/prot.php
@@ -4,7 +4,50 @@
 
 <?php include('templates/galleryProt.php'); ?>
 
+<div class="protContainer">
 
+    <h1>Protein Bez.</h1>
+
+
+    <!-- Img -->
+    <div class="protTabImg"> </div>
+
+    <!-- Nährstoffe -->
+    <div class="naherstoffe">
+        <h2>Nährstoffe</h2>
+
+        <!-- Vitamine -->
+        <h3>Vitamine</h3>
+        <ul>
+            <li>
+                <p>Vitamin A: 800 microgramm auf 100g</p>
+            </li>
+        </ul>
+
+        <!-- Mineralstoffe -->
+        <h3>Mineralstoffe</h3>
+        <ul>
+            <li>
+                <p>Kalium: 373mg auf 100g</p>
+            </li>
+        </ul>
+
+        <!-- Spurenelemente -->
+        <h3>Spurenelemente</h3>
+        <ul>
+            <li>
+                <p>Zink: 0.1mg auf 100g</p>
+            </li>
+        </ul>
+    </div>
+
+    <!-- Tier Art -->
+    <div class="tierArt">
+        <h2>Tier Art </h2>
+        <p>Text</p>
+    </div>
+
+</div>
 
 
 <?php include('templates/footer.php'); ?>
