8b15707ba1aff3e9e79face5bb40ad2bc221cca8
Sven Weidenmann <sven.weidenmann@1up.io>
2022-08-19 13:31:04 +0200

    eleventh commit

--- public/vegi.php
+++ public/vegi.php
@@ -4,7 +4,42 @@
 
 <?php include('templates/galleryVegi.php'); ?>
 
+<div class="vegContainer">
 
+<h1>Vegi Bez.</h1>
+
+<div class="vegTabImg"> </div>
+
+<div class="naherstoffe">
+<h2>Nährstoffe</h2>
+    <h3>Vitamine</h3>
+<ul>
+    <li>
+        <p>Vitamin A: 800 microgramm auf 100g</p>
+    </li>
+</ul>
+    <h3>Mineralstoffe</h3>
+    <ul>
+        <li>
+            <p>Kalium: 373mg auf 100g</p>
+        </li>
+    </ul>
+
+    <h3>Spurenelemente</h3>
+    <ul>
+        <li>
+            <p>Zink: 0.1mg auf 100g</p>
+        </li>
+    </ul>
+
+</div>
+
+<div class="pflanzenArt">
+    <h2>Pflanzen Art </h2>
+    <p>Text</p>
+</div>
+
+</div>
 
 
 <?php include('templates/footer.php'); ?>
