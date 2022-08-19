23af02afaac0bb294015589df6347102189a1bb3
Sven Weidenmann <sven.weidenmann@1up.io>
2022-08-19 13:34:56 +0200

    eleventh commit

--- public/vegi.php
+++ public/vegi.php
@@ -8,16 +8,23 @@
 
 <h1>Vegi Bez.</h1>
 
+
+<!-- Img -->
 <div class="vegTabImg"> </div>
 
+<!-- Nährstoffe -->
 <div class="naherstoffe">
 <h2>Nährstoffe</h2>
+
+<!-- Vitamine -->
     <h3>Vitamine</h3>
-<ul>
-    <li>
-        <p>Vitamin A: 800 microgramm auf 100g</p>
-    </li>
-</ul>
+    <ul>
+        <li>
+            <p>Vitamin A: 800 microgramm auf 100g</p>
+        </li>
+    </ul>
+
+<!-- Mineralstoffe -->
     <h3>Mineralstoffe</h3>
     <ul>
         <li>
@@ -25,19 +32,20 @@
         </li>
     </ul>
 
+<!-- Spurenelemente -->
     <h3>Spurenelemente</h3>
     <ul>
         <li>
             <p>Zink: 0.1mg auf 100g</p>
         </li>
     </ul>
-
 </div>
 
-<div class="pflanzenArt">
-    <h2>Pflanzen Art </h2>
-    <p>Text</p>
-</div>
+<!-- Pflanzen Art -->
+    <div class="pflanzenArt">
+        <h2>Pflanzen Art </h2>
+        <p>Text</p>
+    </div>
 
 </div>
 
