0051aaf18c1c3a9c5642021daa1808f8d06012fa
Sven Weidenmann <sven.weidenmann@1up.io>
2022-08-23 12:01:45 +0200

    diaSlide

--- public/diaAnimation.js
+++ public/diaAnimation.js
@@ -5,13 +5,23 @@ function plusDivs(n) {
     showDivs(slideIndex += n);
 }
 
+function currentDiv(n) {
+    showDivs(slideIndex = n);
+}
+
 function showDivs(n) {
     var i;
     var x = document.getElementsByClassName("burger");
+    var dots = document.getElementsByClassName("border");
     if (n > x.length) {slideIndex = 1}
     if (n < 1) {slideIndex = x.length} ;
     for (i = 0; i < x.length; i++) {
         x[i].style.display = "none";
     }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" white", "");
+    }
     x[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " white";
 }
+
--- public/index.php
+++ public/index.php
@@ -8,22 +8,21 @@
     <img class="dia" src="Bilder/milchprodukt.jpg" alt="Protein">
 
     <div class="bottommiddle" style="width:100%">
-        <button class="button-left" onclick="plusDivs(-1)">&#10094;</button>
-        <button class="button-right" onclick="plusDivs(+1)">&#10095;</button>
-        <span class="border" onclick="currentDiv(1)"></span>
-        <span class="border" onclick="currentDiv(2)"></span>
-        <span class="border" onclick="currentDiv(3)"></span>
+        <div class="button-left" onclick="plusDivs(-1)">&#10094;</div>
+        <div class="dot-nav">
+            <div class="dot" onclick="currentDiv(1)"></div>
+            <div class="dot" onclick="currentDiv(2)"></div>
+            <div class="dot" onclick="currentDiv(3)"></div>
+        </div>
+        <div class="button-right" onclick="plusDivs(+1)">&#10095;</div>
     </div>
 </div>
-
-
 
 <div class="flex-container">
     <a class="gallery" target="_blank" href="Bilder/brokkoli.jpg" style="background-image: url('Bilder/brokkoli.jpg')"></a>
     <a class="gallery" target="_blank" href="Bilder/karotten.jpg" style="background-image: url('Bilder/karotten.jpg')"></a>
     <a class="gallery" target="_blank" href="Bilder/apfel.jpg" style="background-image: url('Bilder/apfel.jpg')"></a>
     <a class="gallery" target="_blank" href="Bilder/orange.jpg" style="background-image: url('Bilder/orange.jpg')"></a>
-
 
     <a class="gallery" target="_blank" href="Bilder/haferflocken.jpg" style="background-image: url('Bilder/haferflocken.jpg')"></a>
     <a class="gallery" target="_blank" href="Bilder/weisserreis.jpg" style="background-image: url('Bilder/weisserreis.jpg')"></a>
